765bd74c6c0863a41aacc66666822f420c1ea106
lahirunirmalx <lahirunirmalx@gamil.com>
2023-04-25 18:36:48 +0530

    add new code

--- src/TelegramClient.php
+++ src/TelegramClient.php
@@ -31,6 +31,7 @@ public function unRegisterChat($chatId){
             'photo' =>'sendPhoto',
             'video' =>'sendVideo',
             'animation' =>'sendAnimation',
+            'text' =>'sendMessage',
         );
 
         $bot_url = "https://api.telegram.org/bot$apiToken/";
@@ -69,78 +70,6 @@ public function unRegisterChat($chatId){
 
     }
 
-    function dumpToBotPhoto($originalFile, $apiToken, $chat_id)
-    {
-
-        $bot_url = "https://api.telegram.org/bot$apiToken/";
-        $url = $bot_url . "sendPhoto?chat_id=" . $chat_id;
-
-        $post_fields = array(
-            'chat_id' => $chat_id,
-            'photo' => new CURLFile(realpath($originalFile))
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type:multipart/form-data"
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        $output = curl_exec($ch);
-
-        $this->getUploadedFileId($output,null,$originalFile);
-        curl_close($ch);
-    }
-
-    function dumpToBotSendVideo($originalFile, $apiToken, $chat_id, $message)
-    {
-
-        $bot_url = "https://api.telegram.org/bot$apiToken/";
-        $url = $bot_url . "sendVideo?chat_id=" . $chat_id;
-
-        $post_fields = array(
-            'chat_id' => $chat_id,
-            'video' => new CURLFile(realpath($originalFile)),
-            'caption' => $message
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type:multipart/form-data"
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        $output = curl_exec($ch);
-
-        $this->getUploadedFileId($output);
-        curl_close($ch);
-    }
-
-    function dumpToBotsendAnimation($originalFile, $apiToken, $chat_id)
-    {
-
-        $bot_url = "https://api.telegram.org/bot$apiToken/";
-        $url = $bot_url . "sendAnimation?chat_id=" . $chat_id;
-
-        $post_fields = array(
-            'chat_id' => $chat_id,
-            'animation' => new CURLFile(realpath($originalFile))
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type:multipart/form-data"
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        $output = curl_exec($ch);
-
-        $this->getUploadedFileId($output,null,$originalFile);
-        curl_close($ch);
-    }
 
 
     function getUploadedFileId($output,$type,$originalFile)
